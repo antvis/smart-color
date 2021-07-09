d72ec177e47feca5ce8e6323b494e085dbd41018
jiazhewang <neoddish@outlook.com>
2021-07-09 23:59:06 +0800

    chore: webpack static site gh-pages

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,83 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const prodConfig = {
-  mode: 'production',
-  devtool: 'cheap-module-source-map',
-  entry: {
-    app: './src/index',
-  },
-  output: {
-    library: 'SmartColorDemo',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist/'),
-    filename: 'index.js',
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.json'],
-    mainFields: ['module', 'browser', 'main'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-typescript', ['@babel/preset-env', { modules: 'commonjs' }]],
-          plugins: [
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-export-default-from',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-object-rest-spread',
-          ],
-        },
-      },
-      {
-        test: /\.(t|j)sx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-typescript', ['@babel/preset-env', { modules: 'commonjs' }], '@babel/preset-react'],
-          plugins: [
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-object-rest-spread',
-          ],
-        },
-      },
-      {
-        test: /\.(css|less)$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader', options: { sourceMap: true } },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true,
-              lessOptions: {
-                javascriptEnabled: true,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
-
 const devConfig = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
@@ -85,7 +8,7 @@ const devConfig = {
     app: './src/index',
   },
   output: {
-    path: path.join(__dirname, './dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
     chunkFilename: '[chunkhash].js',
     publicPath: '/',
@@ -160,10 +83,4 @@ const devConfig = {
   ],
 };
 
-module.exports = (env, argv) => {
-  if (argv.mode === 'production') {
-    return prodConfig;
-  }
-
-  return devConfig;
-};
+module.exports = devConfig;
