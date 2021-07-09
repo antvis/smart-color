b7dcad52cc55a2b753de434ade326c925d15eccf
jiazhewang <neoddish@outlook.com>
2021-07-09 23:59:06 +0800

    chore: webpack static site gh-pages

--- demo/src/app.tsx
+++ demo/src/app.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { randomColor } from '../../src';
+import { randomColor } from '../../src/generators/random';
 import './index.less';
 
+function ShowRandomColor() {
+  const { model, value } = randomColor();
+  if (model === 'rgb') {
+    const { r, g, b } = value as any;
+    return <div className="color-square" style={{ background: `rgb(${r},${g},${b})` }} />;
+  }
+  return null;
+}
+
 export default function App() {
-  const { r, g, b } = randomColor();
-  return <div className="color-square" style={{ background: `rgb(${r},${g},${b})` }} />;
+  return (
+    <>
+      <h1>smart-color Demo</h1>
+      <h2>random color</h2>
+      <ShowRandomColor />
+    </>
+  );
 }
