aa01b9a2a54b9e6fcf708c2e2b68c749b8ac7ec2
christy <christy05.chen@gmail.com>
2021-07-10 13:19:29 +0800

    fix: hexToColor regex

--- __tests__/unit/getPalette/getPaletteFromString.test.ts
+++ __tests__/unit/getPalette/getPaletteFromString.test.ts
@@ -2,7 +2,7 @@ import { getPaletteFromString } from '@src/index';
 
 describe('import palette', () => {
   test('import palette by string', () => {
-    const palette = getPaletteFromString('#123,#abcd, #123456, #09876557,,, ');
-    expect(palette.colors.length).toBe(4);
+    const palette = getPaletteFromString('#123,#abcd, #123456, #09876557,#eee33,, ');
+    expect(palette.colors.length).toBe(5);
   });
 });
