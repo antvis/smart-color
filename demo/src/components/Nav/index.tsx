4b533b505bfd501f8adca91f8210d57212f89e7c
Christy Chen <christy05.chen@gmail.com>
2021-08-27 16:22:42 +0800

    feat(demo): git icon

--- demo/src/components/Nav/index.tsx
+++ demo/src/components/Nav/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Anchor } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 import FEATURES from '../../constant/features';
 import styles from './index.module.less';
 
 const { Link } = Anchor;
 const Nav = () => (
   <div className={styles.nav}>
-    <h1 className={styles.title}>Smart Color</h1>
-    <h2 className={styles.subtitle}>@antv/smart-color</h2>
+    <div className={styles.header}>
+      <h1 className={styles.title}>Smart Color</h1>
+      <h2 className={styles.subtitle}>@antv/smart-color </h2>
+      <a className={styles.git} href="https://github.com/antvis/smart-color" target="_blank" rel="noreferrer">
+        <GithubOutlined />
+      </a>
+    </div>
+
     <Anchor>
       <Link href={'#intro'} title={'Features'}></Link>
       {FEATURES.map(({ name, url, childeren }) => (
